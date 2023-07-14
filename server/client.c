a67ee1337c14f47052548fa13918fa3a3bc6f6c7
drashie <dan.mey@protonmail.com>
2023-07-14 19:46:38 +0200

    comments for clarification of lib functions

--- server/client.c
+++ server/client.c
@@ -1,23 +1,28 @@
-/* to build: 
-
-    sudo apt install libczmq-dev
-
-     gcc -o client client.c -lczmq
-    ./client */
+/*  
+    1. sudo apt install libczmq-dev     -> get library
+    2. gcc -o client client.c -lczmq    -> compile
+    3. ./client                         -> run
+*/
 
 #include <czmq.h>
 
 int main(int argc, char **argv)
 {
+    /* create socket as requester to connect to server */
     zsock_t *requester = zsock_new(ZMQ_REQ);
+
+    /* connect to server with requester and send data */
     zsock_connect(requester, "tcp://localhost:5555");
     zstr_send(requester, "Low Level");
 
+    /* wait for the server to response */
     sleep(1);
 
+    /* print out response */
     char *str = zstr_recv(requester);
     printf("%s!", str);
 
+    /* close socket */
     zsock_destroy(&requester);
 
     return 0;
--- server/server.c
+++ server/server.c
@@ -1,33 +1,33 @@
-/* to build: 
-
-    sudo apt install libczmq-dev
-
-    gcc -o server server.c -lczmq
-    ./server 
+/*  
+    1. sudo apt install libczmq-dev     -> get library
+    2. gcc -o server server.c -lczmq    -> compile
+    3. ./server                         -> run
 */
 
 #include <czmq.h>
 
 int main(int argc, char **argv)
 {
-    // server, client
-    // request, response
-
+    /* create socket as responder to listen to request */
     zsock_t *responder = zsock_new(ZMQ_REP);
-    int r = zsock_bind(responder, "tcp://*:5555");
 
+    /* try to open port */
+    int r = zsock_bind(responder, "tcp://*:5555");
     if (r != 5555)
         printf("FAILED to bind to port\n");
 
+    /* listen to incoming msg */
     while (true) {
         char *msg = zstr_recv(responder);
 
+        /* incoming msg = Low Level, Repsonse with Gang */
         if (!strcmp(msg, "Low Level"))
             zstr_send(responder, "Gang");
 
         free(msg);
     }
 
+    /* close socket */
     zsock_destroy(&responder);
 
     return 0;
