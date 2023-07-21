b035b73afa3be8b2f2db07da8a49cf7bffd9d092
drashie <dan.mey@protonmail.com>
2023-07-21 10:56:40 +0200

    server communication class init commit

---
+++ client_ui/app/src/main/java/com/example/clientui/ServerCommnunication.kt
@@ -0,0 +1,15 @@
+package com.example.clientui
+
+/* Here the communication with our server happens */
+class ServerCommnunication {
+    /* get the data from the server */
+    fun get_data(): String {
+        return "HELLO WORLD";
+    }
+
+    /* send given data to the server */
+    fun add_data(content: String){
+
+    }
+
+}
