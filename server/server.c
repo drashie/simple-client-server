cca80379f18fb136935f7ff648c4d84236ea2ae9
drashie <dan.mey@protonmail.com>
2023-07-27 15:46:07 +0200

    exception for unknown command

--- server/server.c
+++ server/server.c
@@ -16,6 +16,7 @@ int main(int argc, char **argv)
 {
     /* create socket as responder to listen to request */
     zsock_t *responder = zsock_new(ZMQ_REP);
+    printf("main fn started!\n");
 
     /* try to open port */
     int r = zsock_bind(responder, SERVER);
@@ -23,24 +24,28 @@ int main(int argc, char **argv)
         printf("FAILED to bind to port\n");
         return 4;
     }
+    printf("port is bound to %d\n", r);
 
     /* listen to incoming msg */
     while (true) {
         char *msg = zstr_recv(responder);
+        printf("listening for msgs\n");
         
         if (msg != NULL) {
+            printf("Recv Msg: %s\n", msg);
             if (!strncmp(msg, "GET", MAX_INPUT_BUFFER_SIZE)) {
                 char *output = get_data_content();
                 zstr_send(responder, output);
                 free(output);
-            }
-
-            if ((strlen(msg) < MAX_INPUT_BUFFER_SIZE) && (strstr(msg, "ADD"))) { 
+            } else if ((strlen(msg) < MAX_INPUT_BUFFER_SIZE) && (strstr(msg, "ADD"))) { 
                 char line[MAX_INPUT_BUFFER_SIZE];
                 strcpy(line, (msg + 4)); /* Cut the ADD cmd from the given string */
 
                 if (!write_data_content(line)) /* try to add the line to the txt file */
                     zstr_send(responder, "LINE SUCCESFULLY ADDED");
+            } else {
+                printf("Unknown Command!: %s\n", msg);
+                zstr_send(responder, "ERROR: UNKNOWN CMD!");
             }
         }
 
