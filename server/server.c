c8a5e8e29db6cf352d7831b3d7b724ab3504fe6d
drashie <dan.mey@protonmail.com>
2023-07-30 17:25:11 +0200

    add server cmds and responses back

--- server/server.c
+++ server/server.c
@@ -15,6 +15,33 @@
 
 #define PORT 5555
 
+/**
+ * @brief process msg received from client
+ *        and return server response depending
+ *        on the found command
+ * @param msg from client
+ * @return response depending on msg command 
+ */
+char* msg_processing(char* msg)
+{
+    char *output = NULL;
+    if (!strncmp(msg, "GET", MAX_INPUT_BUFFER_SIZE))
+        output = get_data_content();
+    else if ((strlen(msg) < MAX_INPUT_BUFFER_SIZE) && (strstr(msg, "ADD"))) {
+        char line[MAX_INPUT_BUFFER_SIZE];
+        strcpy(line, (msg + 4)); /* cut the add command from the string */
+
+        if (!write_data_content(line)) /* try to add the line to the string */
+            output = strdup("LINE SUCCESSFULLY ADDED");
+        else {
+            perror("failed to write to file!");
+            exit(EXIT_FAILURE);
+        }
+    } else 
+        output = strdup("ERROR: UNKNOWN CMD!");
+    return output;
+}
+
 int main(int argc, char **argv)
 {
     int serverSocket, clientSocket;
@@ -27,6 +54,7 @@ int main(int argc, char **argv)
         perror("failed to create socket!");
         exit(EXIT_FAILURE);
     }
+    /* allow immediat reuse of ip/port after process termination */
     int enableImmediatReuseOfPort = 1;
     if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &enableImmediatReuseOfPort, sizeof(enableImmediatReuseOfPort)) < 0) {
         perror("set socket settings failed!");
@@ -42,7 +70,6 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
     printf("bound to port: %d\n", PORT);
-
 
     if (listen(serverSocket, 1) != -1) {
         /* listen to incoming connections and msg */
@@ -60,8 +87,10 @@ int main(int argc, char **argv)
                 buffer[bytesRead] = '\0';
                 printf("Received from client: %s\n", buffer);
 
-                const char *response = "Hello from server!";
-                send(clientSocket, response, strlen(response), 0);
+                char *resp = msg_processing(buffer);
+                send(clientSocket, resp, strlen(resp), 0);
+                free(resp);
+                printf("buffer freed!\n");
             }
             close(clientSocket);
         }
